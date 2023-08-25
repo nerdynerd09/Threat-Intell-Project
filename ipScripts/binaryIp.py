6b744386d279e61c50c1d8ce18f18b1b4b1f59c4
nerdynerd09 <nerdynerd0905@gmail.com>
2023-08-25 13:42:41 +0530

    adding ips

---
+++ ipScripts/binaryIp.py
@@ -0,0 +1,22 @@
+import requests,re
+from bs4 import BeautifulSoup
+from dbFile import dbStore
+
+def binaryIPCollector():
+     
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0',
+        # Requests doesn't support trailers
+        # 'TE': 'trailers',
+    }
+
+    url = "https://www.binarydefense.com/banlist.txt"
+
+    resp = requests.get(url,headers=headers)
+    bsContent = BeautifulSoup(resp.text,"lxml")
+    
+    result = re.findall(r'[0-9].*',str(bsContent))
+    resultList =  [[i,"NA"] for i in result]
+    
+    dbStore(resultList=resultList)
+    print("Done")
