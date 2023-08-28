f9858f4bb429c1f3859b2e11ac939c94cfa7788f
vikashkumar040203 <vikashkumar990523@gmail.com>
2023-08-28 11:49:32 +0530

    ip-done

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+from ipScripts.dbFile import dbSearch
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def threat_intel():
+    result = None
+    if request.method == 'POST':
+        ip = request.form.get('q')
+        result = dbSearch(ip)
+    return render_template('home.html', result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ ipScripts/dbFile.py
@@ -0,0 +1,14 @@
+import pymongo
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["iitk"]
+mycol = mydb["threatintell"]
+def dbStore(resultList):
+    # print(result)
+    for i in resultList:
+        if mycol.find_one({"ip":i[0]}) is None:
+            mycol.insert_one({"ip":i[0],"location":i[1]})
+def dbSearch(target):
+    if mycol.find_one({"ip": target}) is None:
+        return "Non-malicious IP"
+    else:
+        return "Malicious IP"
